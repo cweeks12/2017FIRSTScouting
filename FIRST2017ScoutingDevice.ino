842d72a7dadf9d8cc53c657df52694faba829160
Connor Weeks <connorweeks1@gmail.com>
2017-02-23 09:30:12 -0700

    Fixed #define issues and added to README

--- FIRST2017ScoutingDevice.ino
+++ FIRST2017ScoutingDevice.ino
@@ -9,16 +9,17 @@
 #define TELEOP_TICKS (TELEOP_TIME_IN_SECONDS + EXTRA_TIME_TO_INPUT) * 1000 / TICK_PERIOD
 
 #define NUMBER_OF_BUTTONS 5
-#define BUTTON_5 3
-#define BUTTON_4 13
-#define BUTTON_3 6
-#define BUTTON_2 5
-#define BUTTON_1 4
-#define LED_1 A0
-#define LED_2 A1
-#define LED_3 A2
-#define LED_4 A3
-#define LED_5 A4
+// These can't be #defines because apparently they're redefined somewhere else
+const uint8_t BUTTON_FOUR = 13;
+const uint8_t BUTTON_THREE = 6;
+const uint8_t BUTTON_TWO = 5;
+const uint8_t BUTTON_ONE = 4;
+const uint8_t BUTTON_ZERO = 3;
+#define LED_ZERO A0
+#define LED_ONE A1
+#define LED_TWO A2
+#define LED_THREE A3
+#define LED_FOUR A4
 #define GO_BUTTON 0
 #define LCD_RS 7
 #define LCD_ENABLE 8
@@ -40,7 +41,7 @@ LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
    wiper to LCD VO pin (pin 3)
 */
 
-uint8_t buttonArray[NUMBER_OF_BUTTONS] = [BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4, BUTTON_5];
+uint8_t buttonArray[NUMBER_OF_BUTTONS] = {BUTTON_ZERO, BUTTON_ONE, BUTTON_TWO, BUTTON_THREE, BUTTON_FOUR};
 
 // Reads a button
 // They're set up in pull-down orientation, so a reading of LOW is the button pushed
@@ -121,16 +122,16 @@ void MM_tick() {
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(BUTTON_5, INPUT_PULLUP);
-  pinMode(BUTTON_4, INPUT_PULLUP);
-  pinMode(BUTTON_3, INPUT_PULLUP);
-  pinMode(BUTTON_2, INPUT_PULLUP);
-  pinMode(BUTTON_1, INPUT_PULLUP);
-  pinMode(LED_5, OUTPUT);
-  pinMode(LED_4, OUTPUT);
-  pinMode(LED_3, OUTPUT);
-  pinMode(LED_2, OUTPUT);
-  pinMode(LED_1, OUTPUT);
+  pinMode(BUTTON_FOUR, INPUT_PULLUP);
+  pinMode(BUTTON_THREE, INPUT_PULLUP);
+  pinMode(BUTTON_TWO, INPUT_PULLUP);
+  pinMode(BUTTON_ONE, INPUT_PULLUP);
+  pinMode(BUTTON_ZERO, INPUT_PULLUP);
+  pinMode(LED_FOUR, OUTPUT);
+  pinMode(LED_THREE, OUTPUT);
+  pinMode(LED_TWO, OUTPUT);
+  pinMode(LED_ONE, OUTPUT);
+  pinMode(LED_ZERO, OUTPUT);
 
   lcd.begin(16, 2);
   lcd.print("Waiting to begin");
@@ -139,19 +140,19 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (buttonRead(BUTTON_4)) {
+  if (buttonRead(BUTTON_THREE)) {
     Serial.println("I\tlove\tyou");
   }
   MM_tick();
-  uint8_t three = buttonRead(BUTTON_3);
-  digitalWrite(LED_3, three);
-  uint8_t four = buttonRead(BUTTON_4);
-  digitalWrite(LED_4, four);
-  uint8_t two = buttonRead(BUTTON_2);
-  digitalWrite(LED_2, two);
-  uint8_t one = buttonRead(BUTTON_1);
-  digitalWrite(LED_1, one);
-  uint8_t five = buttonRead(BUTTON_5);
-  digitalWrite(LED_5, five);
+  uint8_t three = buttonRead(BUTTON_TWO);
+  digitalWrite(LED_TWO, three);
+  uint8_t four = buttonRead(BUTTON_THREE);
+  digitalWrite(LED_THREE, four);
+  uint8_t two = buttonRead(BUTTON_ONE);
+  digitalWrite(LED_ONE, two);
+  uint8_t one = buttonRead(BUTTON_ZERO);
+  digitalWrite(LED_ZERO, one);
+  uint8_t five = buttonRead(BUTTON_FOUR);
+  digitalWrite(LED_FOUR, five);
   delay(TICK_PERIOD);
 }
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 2017FIRSTScouting
+There are two parts to this:
+
+1. The .ino with the Arduino code. I'll also include a schematic of the completed Arduino setup and pictures!
+	The code has two state machines in it, a master that controls the state of the device, whether it's in data entry mode, autonomous mode, tele-op mode, data sync mode, and wait mode. The second machine handles the scoring. It changes modes whether it's in autonomous mode or tele-op mode.
+
+2. The Python script that interfaces with the serial port on the Arduino.
+	This is forthcoming
